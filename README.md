4efc165cec67aac3a8f724d3a030c5960cd80669
yumenghe <yhe54@u.rochester.edu>
2024-06-03 16:28:53 +0800

    add scripts for automatic test and scaling

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # GLITCH_scripts
+
+To run comp_script.sh, which takes a STL file as input and visualizes the generated heatmap on local machine:
+
+    chmod +x comp_script.sh
+    ./comp_script.sh [stlfile] [rotx] [roty] [rotz] [sampling-gap] [center-x] [center-y] [boxsize-x] [boxsize-y] [boxsize-z] [percentile]
+
+To run stlscale, which scales a STL file to fit the given build volume:
+    
+    python stlscale [stlfilepath]
+
+Note: `stlscale` utilizes the `fits` method from `stlinfo`. This `fits` method will also return true if there is a possible rotation of the model that allows it to fit on the build plate. Instead, `fits` in stlscale should only return true if the model can fit without rotation.
---
+++ comp_script.sh
@@ -0,0 +1,47 @@
+#!/bin/bash  --login
+FILE_1=""
+FILE_2=""
+
+export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
+export PATH=$PATH:/Users/yumenghe/Library/Python/3.10/bin
+export PATH=$PATH:/Applications/UltiMaker\ Cura.app/Contents/MacOS
+
+output=$(stlinfo "/Users/yumenghe/Desktop/stl_files/$1")
+height=$(echo $output | awk -F'Z: ' '{print $2}' | awk '{print $1}')
+center=$(echo "$height" | awk '{printf "%.1f", $1 / 2}')
+
+pj3d test1 create "Voron 0" /Users/yumenghe/Library/print_settings.txt
+pj3d test1 add "/Users/yumenghe/Desktop/stl_files/$1"
+pj3d test1 pack
+pj3d test1 print
+
+pj3d test2 create "Voron 0" /Users/yumenghe/Library/print_settings.txt
+pj3d test2 add "/Users/yumenghe/Desktop/stl_files/$1"
+pj3d test2 pack
+pj3d test2 printpart --rotxyz "$2","$3","$4" --all
+
+for file in "test1.job"/*.gcode; do
+    if [ -f "$file" ]; then
+        FILE_1="$file"
+        break
+    fi
+done
+
+for file in "test2.job"/*.gcode; do
+    if [ -f "$file" ]; then
+        FILE_2="$file"
+        break
+    fi
+done
+
+scp "$FILE_1" "$FILE_2" yhe54@capella:Gcode-Checking-Project/gcode/
+rm -r test1.job
+rm -r test2.job
+
+ssh -t capella "conda activate Gcode-comparer; cd Gcode-Checking-Project; python gcode_comp_Z.py 'gcode/$(basename $FILE_1)' 'gcode/$(basename $FILE_2)' '$(echo $2 | bc -l)' '$(echo $3 | bc -l)' '$(echo $4 | bc -l)' '$center' -g '$(echo $5 | bc -l)' -c '$(echo $6 | bc -l)' '$(echo $7 | bc -l)' --cubesize '$(echo ${8} | bc -l)' '$(echo ${9} | bc -l)' '$(echo ${10} | bc -l)' -p '$(echo ${11} | bc -l)'"
+
+scp yhe54@capella:Gcode-Checking-Project/heatmap.pcd /Users/yumenghe/
+# scp yhe54@capella:Gcode-Checking-Project/visualization_two_pcds.pcd /Users/yumenghe/
+conda activate gc
+cd read_pcd
+python main.py /Users/yumenghe/heatmap.pcd
