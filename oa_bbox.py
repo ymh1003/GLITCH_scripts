8dad48ea84284ee81a1ace00f623e0324e8045a5
yumenghe <yhe54@u.rochester.edu>
2024-08-18 23:36:00 -0600

    support case that original model cannot slice

--- oa_bbox.py
+++ oa_bbox.py
@@ -8,9 +8,9 @@ import re
 from pathlib import Path
 import numpy as np
 import math
-import sys
+import json
 
-def triple(arg):
+def parse_str(arg):
     return [float(n) for n in arg.split(",")]
 
 def extract_MinX(input_line):
@@ -61,7 +61,15 @@ def rotation_matrix(agl):
     # in cura need to negate the y angle
     rot_y = np.asarray([[math.cos(agl[1]), 0, math.sin(-agl[1])], [0, 1, 0], [-math.sin(-agl[1]), 0, math.cos(agl[1])]])
     rot_z = np.asarray([[math.cos(agl[2]), -math.sin(agl[2]), 0], [math.sin(agl[2]), math.cos(agl[2]), 0], [0, 0, 1]])
-    return rot_x, rot_y, rot_z
+    return np.matmul(rot_z, np.matmul(rot_y, rot_x))
+
+def rotate_points(pts, rot, ct, undo):
+    agl = np.radians(rot)
+    rot_mat = rotation_matrix(agl).transpose() if undo else rotation_matrix(agl)
+    translation = np.transpose(pts - np.array(ct))
+    rotated = np.matmul(rot_mat, translation).transpose() + ct
+    rotated[:, -1] -= np.min(rotated[:, -1])
+    return rotated
 
 # compute overall bbox from a list of bounding boxes
 def overall_bbox(bbox_list):
@@ -72,48 +80,44 @@ def overall_bbox(bbox_list):
     return [[min(coord_dict[f'c{i}']) for i in range(3)],
             [max(coord_dict[f'c{i}']) for i in range(3, 6)]]
 
+def align_bbox(bbox_1, rot_1, center_1, rot_2, center_2):
+    undo_rotated = rotate_points(bbox_1, rot_1, center_1, undo=1)
+    if rot_2 == [0., 0., 0.]:
+        rotated = undo_rotated
+    else:
+        rotated = rotate_points(undo_rotated, rot_2, center_2, undo=0)
 
-def align_bbox(bbox_1, bbox_2, rot, center):
-    agl = np.radians(rot)
-    agl_back = [-a for a in agl]
-
-    bbox_1 = np.array(bbox_1)
-    rot_x, rot_y, rot_z = rotation_matrix(agl)
-    translation = np.transpose(bbox_1 - center)
-    rotated = np.matmul(rot_z, np.matmul(rot_y, np.matmul(rot_x, translation))).transpose() + center
-    min_z = np.min(rotated[:, -1])
-    print(min_z)
-    
-    bbox_2 = np.array(bbox_2)
-    bbox_2[:, -1] += min_z
-    rot_x, rot_y, rot_z = rotation_matrix(agl_back)
-    translation = np.transpose(bbox_2 - center)
-    rotated = np.matmul(rot_x, np.matmul(rot_y, np.matmul(rot_z, translation))).transpose() + center
-    min_z = np.min(rotated[:, -1])
-    print(min_z)  # should be approximately 0
-    
-    rotated[:, -1] -= min_z
-    
     bmin = rotated[0] if rotated[0][0] < rotated[1][0] else rotated[1]
     bmax = rotated[1] if rotated[0][0] < rotated[1][0] else rotated[0]
+    
     return [bmin, bmax]
 
-# e.g., oa_bbox.py gcode1 150,150,5 -p gcode2 90,0,0 -p gcode3 0,90,0
+    
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description="Compute overall bounding box")
-    p.add_argument("orig_gcode", help="G-code for the original orientation")
-    p.add_argument("center", help="rotation center in x,y,z")
-    p.add_argument("-p", "--pair", action="append", nargs=2, metavar=('G-code', 'rotation'),
-                   help="Specify a pair of g-code and its rotation")
+    p.add_argument("gcode")
+    p.add_argument("rot", default="0,0,0", type=parse_str,
+                   help="rotation applied on the original model to get sliced")
+    p.add_argument("center", type=parse_str, help="xy coordinates of rotation center")
+    p.add_argument("--height", type=float, help="provide the height of the original model if gcode is not original")
+    p.add_argument("-t", "--triple", action="append", nargs=3,
+                   metavar=('G-code', 'rotation', 'height'),
+                   help="Specify a triple of g-code, rotation, and height")
     args = p.parse_args()
     
-    orig_bbox = parse_bbox(args.orig_gcode)
+    orig_bbox = parse_bbox(args.gcode)
     bb_list = [orig_bbox]
-    for gc, rot in args.pair:
-        bbox = parse_bbox(gc)
-        rot_bbox = align_bbox(orig_bbox, bbox, triple(rot), triple(args.center))
-        bb_list.append(rot_bbox)
     
-    sys.exit(overall_bbox(bb_list))
+    if args.triple is not None:
+        for gc, rot, h in args.triple:
+            bbox = parse_bbox(gc)
+            center_back = args.center + [float(h) / 2]
+            center_fw = None if args.height is None else args.center + [args.height / 2]
+            rot_bbox = align_bbox(bbox, parse_str(rot), center_back, 
+                                args.rot, center_fw)
+            bb_list.append(rot_bbox)
+    
+    bbox_json = json.dumps(overall_bbox(bb_list), indent=4)
+    print(bbox_json)
     
     
