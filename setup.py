ef6fcbde7dc749233b359fb217b196eac5f14747
Sreepathi Pai <sree@cs.rochester.edu>
2025-06-20 14:16:53 -0400

    Add setup.py to install scripts

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='GLITCH-scripts',
+    version='0.0.1',
+    install_requires=[],
+    scripts=['glitch_runner.py', 'oa_bbox.py', 'stlrotate', 'stlscale', 'visualize_pcd.py']
+)
