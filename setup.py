65799c2f5b0e65c59edaab4469288385ca395697
Sreepathi Pai <sree@cs.rochester.edu>
2025-06-23 16:36:06 -0400

    Fix setup.py issue

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='GLITCH-scripts',
     version='0.0.1',
     install_requires=[],
+    packages=[],
     scripts=['glitch_runner.py', 'oa_bbox.py', 'stlrotate', 'stlscale', 'visualize_pcd.py']
 )
