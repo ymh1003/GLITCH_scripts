b01da7a565b9bcd5310a80a9abf5f7f4e506811f
Sreepathi Pai <sree@cs.rochester.edu>
2025-07-19 10:20:04 -0400

    Time visualize_pcd as well

--- time_make.sh
+++ time_make.sh
@@ -4,6 +4,6 @@ set -e
 
 LF=log-`date +%Y%m%d-%H%M%S`.log
 echo "*** TIME: " "make $@" >> $LF
-make GLITCH_RUNNER="/usr/bin/time -p glitch_runner.py" GCODE_COMP_Z="/usr/bin/time -p gcode_comp_Z.py" "$@" 2>&1 | tee -a $LF
+make GLITCH_RUNNER="/usr/bin/time -p glitch_runner.py" GCODE_COMP_Z="/usr/bin/time -p gcode_comp_Z.py" "$@" VISUALIZE_PCD="/usr/bin/time -p visualize_pcd.py" 2>&1 | tee -a $LF
 echo "*** SUCCESS"
 
