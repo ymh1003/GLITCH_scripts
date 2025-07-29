3db37e4f60e1a28b9031cafb3c15bc6bfae08845
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-07-28 18:42:55 -0700

    change size of plot

--- glitchplot_log.py
+++ glitchplot_log.py
@@ -9,7 +9,7 @@ normal = df[df["Ratio"] <= 1]
 normal_color = 'green'
 outlier_color = 'orange'
 
-plt.figure(figsize=(12, 5))
+plt.figure(figsize=(6, 6))
 
 plt.scatter(normal["Print time"], normal["Analysis time"], alpha=0.7,
             color=normal_color)
@@ -32,6 +32,6 @@ plt.legend()
 plt.grid(True, which="both", ls="--", linewidth=0.5)
 
 plt.tight_layout(w_pad=5)
-plt.savefig("benchmark_analysis_plots.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("runningtimes.pdf", format="pdf", bbox_inches="tight")
 plt.show()
 
