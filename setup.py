a909ee59346da53684814c4a986cadaab764edfb
Sreepathi Pai <sree@cs.rochester.edu>
2025-07-29 17:52:26 -0400

    Add prep_html_index.py to path

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.0.1',
     install_requires=[],
     packages=[],
-    scripts=['glitch_runner.py', 'oa_bbox.py', 'stlrotate', 'stlscale', 'visualize_pcd.py', 'time_make.sh', 'glitchplot_log.py', 'merge_and_average_runtimes.py', 'process_artifact_logs.py']
+    scripts=['glitch_runner.py', 'oa_bbox.py', 'stlrotate', 'stlscale', 'visualize_pcd.py', 'time_make.sh', 'glitchplot_log.py', 'merge_and_average_runtimes.py', 'process_artifact_logs.py', 'prep_html_index.py']
 )
